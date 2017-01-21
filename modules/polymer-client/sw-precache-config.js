3ab0adec72e543d202400c2eb046bce2dd9019e4
krivopustov <krivopustov@haulmont.com>
2017-01-21 20:54:46 +0400

    Upgrade to 6.4.0.RC0; app icons

--- modules/polymer-client/sw-precache-config.js
+++ modules/polymer-client/sw-precache-config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  replacePrefix: '/app-polymer',
+  replacePrefix: '/app-front',
   staticFileGlobs: [
     'index.html',
     'src/**.html',
+    'images/*',
     'manifest.json',
-    'bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    'bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    'bower_components/fetch/fetch.js'
   ],
   navigateFallback: 'index.html'
 };
