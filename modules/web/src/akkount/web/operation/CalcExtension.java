604275c0ef396dc2a0fd19f1f6036b444e65c8b1
Konstantin Krivopustov <krivopustov@haulmont.com>
2019-03-16 13:02:05 +0400

    Load jquery from webjar

--- modules/web/src/akkount/web/operation/CalcExtension.java
+++ modules/web/src/akkount/web/operation/CalcExtension.java
@@ -1,10 +1,12 @@
 package akkount.web.operation;
 
+import com.haulmont.cuba.web.widgets.WebJarResource;
 import com.vaadin.annotations.JavaScript;
 import com.vaadin.server.AbstractJavaScriptExtension;
 import com.vaadin.ui.TextField;
 
 @JavaScript("textfieldcalc.js")
+@WebJarResource("jquery:jquery.min.js")
 public class CalcExtension extends AbstractJavaScriptExtension {
 
     public CalcExtension(TextField textField) {
