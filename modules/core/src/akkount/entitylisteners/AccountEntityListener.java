c9e1ae45f8c11123db5590ff7b426da6ff1299ea
Konstantin Krivopustov <krivopustov@haulmont.com>
2020-01-05 18:24:15 +0400

    Fix unfetched attribute exception on creating account

--- modules/core/src/akkount/entitylisteners/AccountEntityListener.java
+++ modules/core/src/akkount/entitylisteners/AccountEntityListener.java
@@ -1,24 +1,29 @@
 package akkount.entitylisteners;
 
 import akkount.entity.Account;
+import akkount.entity.Currency;
+import com.haulmont.cuba.core.EntityManager;
 import com.haulmont.cuba.core.listener.BeforeInsertEntityListener;
 import com.haulmont.cuba.core.listener.BeforeUpdateEntityListener;
-import com.haulmont.cuba.core.EntityManager;
 
 public class AccountEntityListener
         implements BeforeInsertEntityListener<Account>, BeforeUpdateEntityListener<Account> {
 
     @Override
     public void onBeforeInsert(Account entity, EntityManager entityManager) {
-        setCurrencyCode(entity);
+        setCurrencyCode(entity, entityManager);
     }
 
     @Override
     public void onBeforeUpdate(Account entity, EntityManager entityManager) {
-        setCurrencyCode(entity);
+        setCurrencyCode(entity, entityManager);
     }
 
-    private void setCurrencyCode(Account account) {
-        account.setCurrencyCode(account.getCurrency().getCode());
+    private void setCurrencyCode(Account account, EntityManager entityManager) {
+        Currency currency = entityManager.find(Currency.class, account.getCurrency().getId());
+        if (currency == null) {
+            throw new RuntimeException("Currency not found: " + account.getCurrency());
+        }
+        account.setCurrencyCode(currency.getCode());
     }
 }
