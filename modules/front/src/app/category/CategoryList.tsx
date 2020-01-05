d88d131b18d7a007355bca981b562bff875db433
Konstantin Krivopustov <krivopustov@haulmont.com>
2020-01-05 18:31:14 +0400

    Frontend: categories list

--- modules/front/src/app/category/CategoryList.tsx
+++ modules/front/src/app/category/CategoryList.tsx
@@ -1,37 +1,44 @@
 import * as React from "react";
 
-import {observable} from "mobx";
+import { observer } from "mobx-react";
 
-import {observer} from "mobx-react";
+import { Modal, Button, List, Icon, Spin } from "antd";
 
-import {Button, Modal} from "antd";
+import { Category } from "../../cuba/entities/akk$Category";
+import { Link } from "react-router-dom";
 
-import {Category} from "../../cuba/entities/akk$Category";
-import {Link} from "react-router-dom";
+import {
+  collection,
+  injectMainStore,
+  MainStoreInjected,
+  EntityProperty
+} from "@cuba-platform/react";
 
-import {collection, DataTable, injectMainStore, MainStoreInjected} from "@cuba-platform/react";
-
-import {SerializedEntity} from "@cuba-platform/rest";
-import {CategoryManagement} from "./CategoryManagement";
-import {FormattedMessage, injectIntl, WrappedComponentProps} from "react-intl";
+import { SerializedEntity } from "@cuba-platform/rest";
+import { CategoryManagement } from "./CategoryManagement";
+import {
+  FormattedMessage,
+  injectIntl,
+  WrappedComponentProps
+} from "react-intl";
 
 @injectMainStore
 @observer
-class CategoryListComponent extends React.Component<MainStoreInjected & WrappedComponentProps> {
+class CategoryListComponent extends React.Component<
+  MainStoreInjected & WrappedComponentProps
+> {
   dataCollection = collection<Category>(Category.NAME, {
     view: "_local",
-    sort: "-updateTs"
+    sort: "name"
   });
 
   fields = ["name", "description", "catType"];
 
-  @observable selectedRowKey: string | undefined;
-
   showDeletionDialog = (e: SerializedEntity<Category>) => {
     Modal.confirm({
       title: this.props.intl.formatMessage(
-        {id: "management.browser.delete.areYouSure"},
-        {instanceName: e._instanceName}
+        { id: "management.browser.delete.areYouSure" },
+        { instanceName: e._instanceName }
       ),
       okText: this.props.intl.formatMessage({
         id: "management.browser.delete.ok"
@@ -40,82 +47,76 @@ class CategoryListComponent extends React.Component<MainStoreInjected & WrappedC
         id: "management.browser.delete.cancel"
       }),
       onOk: () => {
-        this.selectedRowKey = undefined;
-
         return this.dataCollection.delete(e);
       }
     });
   };
 
   render() {
-    const buttons = [
-      <Link
-        to={CategoryManagement.PATH + "/" + CategoryManagement.NEW_SUBPATH}
-        key="create"
-      >
-        <Button
-          htmlType="button"
-          style={{margin: "0 12px 12px 0"}}
-          type="primary"
-          icon="plus"
+    const { status, items } = this.dataCollection;
+
+    if (status === "LOADING") {
+      return (
+        <div
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%, -50%)"
+          }}
         >
-          <span>
-            <FormattedMessage id="management.browser.create"/>
-          </span>
-        </Button>
-      </Link>,
-      <Link to={CategoryManagement.PATH + "/" + this.selectedRowKey} key="edit">
-        <Button
-          htmlType="button"
-          style={{margin: "0 12px 12px 0"}}
-          disabled={!this.selectedRowKey}
-          type="default"
-        >
-          <FormattedMessage id="management.browser.edit"/>
-        </Button>
-      </Link>,
-      <Button
-        htmlType="button"
-        style={{margin: "0 12px 12px 0"}}
-        disabled={!this.selectedRowKey}
-        onClick={this.deleteSelectedRow}
-        key="remove"
-        type="default"
-      >
-        <FormattedMessage id="management.browser.remove"/>
-      </Button>
-    ];
-
-    return (
-      <DataTable
-        dataCollection={this.dataCollection}
-        fields={this.fields}
-        onRowSelectionChange={this.handleRowSelectionChange}
-        hideSelectionColumn={true}
-        buttons={buttons}
-      />
-    );
-  }
-
-  getRecordById(id: string): SerializedEntity<Category> {
-    const record:
-      | SerializedEntity<Category>
-      | undefined = this.dataCollection.items.find(record => record.id === id);
-
-    if (!record) {
-      throw new Error("Cannot find entity with id " + id);
+          <Spin size="large" />
+        </div>
+      );
     }
 
-    return record;
+    return (
+      <div className="narrow-layout">
+        <div style={{ marginBottom: "12px" }}>
+          <Link
+            to={CategoryManagement.PATH + "/" + CategoryManagement.NEW_SUBPATH}
+          >
+            <Button htmlType="button" type="primary" icon="plus">
+              <span>
+                Create Category
+              </span>
+            </Button>
+          </Link>
+        </div>
+
+        <List
+          itemLayout="horizontal"
+          bordered
+          dataSource={items}
+          renderItem={item => (
+            <List.Item
+              actions={[
+                <Link to={CategoryManagement.PATH + "/" + item.id} key="edit">
+                  <Icon type="edit" />
+                </Link>,
+                <Icon
+                  type="delete"
+                  key="delete"
+                  onClick={() => this.showDeletionDialog(item)}
+                />
+              ]}
+            >
+              <div style={{ flexGrow: 1 }}>
+                {this.fields.map(p => (
+                  <EntityProperty
+                    entityName={Category.NAME}
+                    propertyName={p}
+                    value={item[p]}
+                    key={p}
+                  />
+                ))}
+              </div>
+            </List.Item>
+          )}
+        />
+      </div>
+    );
   }
-
-  handleRowSelectionChange = (selectedRowKeys: string[]) => {
-    this.selectedRowKey = selectedRowKeys[0];
-  };
-
-  deleteSelectedRow = () => {
-    this.showDeletionDialog(this.getRecordById(this.selectedRowKey!));
-  };
 }
 
 const CategoryList = injectIntl(CategoryListComponent);
